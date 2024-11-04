3d5290469bf7d33f5203ca2b041450639bae1a32
Nicole <nicolelogan@hotmail.com>
2024-11-04 14:05:50 -0800

    finished assignment

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,45 @@ const rightButton = document.getElementById('btn-turn-counter-clockwise');
 const numberOfSlides = 34;
 let counter = 0;
 
+// Array of images 
+
+function createImages(numberOfImages, imageName) {
+    const imageList = [];
+    for (let i = 1; i < numberOfImages; i++) {
+        const img = new Image();
+        img.src = `images/${imageName}-${i}.jpg`;
+        imageList.push(img);
+    }
+    return imageList;
+}
+
+// Creating slides
+
+const slides = createImages(numberOfSlides, 'bike');
+
+// Function to change the slide
+
+function turnCW() { 
+
+    counter++;
+    if (counter === numberOfSlides) {
+        counter = 0;
+    }
+
+    slide.src = slides[counter].src;
+}
+
+function turnCCW() {
+
+    counter--;
+    if (counter < 0) {
+        counter = numberOfSlides - 1;
+    }
+
+    slide.src = slides[counter].src;
+}
+
+// Event listeners for buttons! 
+
+leftButton.addEventListener('click', turnCW);
+rightButton.addEventListener('click', turnCCW);
