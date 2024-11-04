f3ac34144fbbb201e4cee23064a4cbc2943adb23
Nicole <nicolelogan@hotmail.com>
2024-11-04 13:43:58 -0800

    setting up spinning bike assignment

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Determine the HTML elements that we'll need to interact with
+
+const slide = document.getElementById('slide');
+const leftButton = document.getElementById('btn-turn-clockwise');
+const rightButton = document.getElementById('btn-turn-counter-clockwise');
+
+// Add event listeners to the buttons
